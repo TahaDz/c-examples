f16f9ab8293d40ca335429fd43a4411410507d75
Taha Rahmani <taha3197@gmail.com>
2017-08-23 11:04:18 +0100

    Add a simple UI using printf

--- sort/sort.c
+++ sort/sort.c
@@ -12,11 +12,11 @@ char *lines_ptr[LINES];
 int main(){
 
 	int nlines = 0;
-	
+	printf("This program sort lines from input & print them sorted :\n \thow to us it :\n\t1- write several lines (each line will end with clicking ENTR button)\n\t2-click on CTRL+D to end the reading\n");
 	if ((nlines = readlines(lines_ptr, LINES)) < LINES){
-
-
+		
 		qsort(lines_ptr, 0, nlines-1);
+		printf("\t\t\tthe sorted lines :\n");
 		write_lines(lines_ptr, nlines);
 	
 	}
